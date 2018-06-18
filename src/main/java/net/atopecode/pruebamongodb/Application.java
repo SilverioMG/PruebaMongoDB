efca8873b0c6a9543300fb0213a50fef855193e2
AtopeCode <atopecode.gmail.com>
2018-06-18 21:13:08 +0200

    Commit 18-06-2018

--- src/main/java/net/atopecode/pruebamongodb/Application.java
+++ src/main/java/net/atopecode/pruebamongodb/Application.java
@@ -1,5 +1,7 @@
 package net.atopecode.pruebamongodb;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
 import net.atopecode.pruebamongodb.config.ApplicationConfig;
 import net.atopecode.pruebamongodb.model.Persona;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -10,10 +12,15 @@ public class Application {
     public static void main(String[] args){
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
         MongoTemplate mongoTemplate = context.getBean(MongoTemplate.class);
+        MongoClient mongoClient = (MongoClient)context.getBean("mongoClient");
 
         try{
-            Persona persona = new Persona("Very");
-            mongoTemplate.save(persona);
+            MongoDatabase db = mongoClient.getDatabase("local");
+            String stringDB = db.toString();
+            System.out.println(stringDB);
+            db.drop();
+            //Persona persona = new Persona("Very");
+            //mongoTemplate.save(persona);
         }
         catch(Exception ex){
             System.out.println(ex.getStackTrace());
--- src/main/java/net/atopecode/pruebamongodb/config/ApplicationConfig.java
+++ src/main/java/net/atopecode/pruebamongodb/config/ApplicationConfig.java
@@ -1,7 +1,10 @@
 package net.atopecode.pruebamongodb.config;
 
+import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.sun.security.ntlm.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -10,31 +13,53 @@ import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoClientFactoryBean;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
 import java.net.UnknownHostException;
+import java.rmi.server.ServerCloneException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @ComponentScan("net.atopecode.pruebamongodb")
+@EnableMongoRepositories("net.atopecode.pruebamongodb")
 public class ApplicationConfig {
 
     public static String getDataBaseName(){
-        return "local";
+        return "admin";
     }
 
     public static String getServerBD(){
         return "localhost";
     }
 
+    public static String getHostBD() { return "127.0.0.1"; }
+    public static int getServerPort(){ return 27017; }
+
     public static UserCredentials getUserCredentials(){
         UserCredentials userCredentials = new UserCredentials("testuser", "testuser");
         return userCredentials;
     }
 
+    public static MongoCredential getMongoCredentials(){
+        MongoCredential mongoCredential = MongoCredential.createCredential("myadmin", getDataBaseName(), "myadmin".toCharArray());
+        return mongoCredential;
+    }
+
+    public static ServerAddress getServerAddress() throws UnknownHostException{
+        ServerAddress serverAddress = new ServerAddress(getHostBD(), getServerPort());
+        return serverAddress;
+    }
+
     /*
      * Use the standard Mongo driver API to create a com.mongodb.MongoClient instance.
      */
-    @Bean
+    @Bean(name = "mongoClient")
     public MongoClient mongoClient() throws UnknownHostException {
-        return new MongoClient("localhost");
+        //return new MongoClient("localhost");
+        List<MongoCredential> credentials = new ArrayList<MongoCredential>();
+        credentials.add(getMongoCredentials());
+        return new MongoClient(getServerAddress(), credentials);
     }
 
     /*
@@ -44,20 +69,20 @@ public class ApplicationConfig {
     public MongoClientFactoryBean mongoClientFactoryBean() {
         MongoClientFactoryBean factoryBean = new MongoClientFactoryBean();
         factoryBean.setHost(getServerBD());
-        MongoCredential mongoCredential = MongoCredential.createCredential("testuser", getDataBaseName(), "testuser".toCharArray());
+        MongoCredential mongoCredential = getMongoCredentials();
         factoryBean.setCredentials(new MongoCredential[]{ mongoCredential });
         return factoryBean;
     }
 
     public @Bean MongoDbFactory mongoDbFactory() throws UnknownHostException {
-        return new SimpleMongoDbFactory(new MongoClient(), getDataBaseName());
+        return new SimpleMongoDbFactory(mongoClient(), getDataBaseName());
     }
 
     @Bean
     public MongoTemplate mongoTemplate() throws Exception {
-        MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory());
-        //TODO... Ver como se añaden las credenciales y como se hace para que la B.D. no admita conexiones anónimas.
-        //MongoTemplate mongoTemplate = new MongoTemplate(mongoClient(), getDataBaseName(), getUserCredentials());
+        MongoDbFactory mongoDbFactory = mongoDbFactory();
+        MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory);
+        //MongoTemplate mongoTemplate1 = new MongoTemplate(mongoClient(), getDataBaseName());
         return mongoTemplate;
     }
 }
